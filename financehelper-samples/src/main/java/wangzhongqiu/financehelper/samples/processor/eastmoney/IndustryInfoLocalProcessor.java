3a06de5f5d467572cf6a43b24567e45ed82db3d9
wangzhongqiu <git.hoomxb.com>
2017-09-14 18:13:52 +0800

    first version

--- financehelper-samples/src/main/java/wangzhongqiu/financehelper/samples/processor/eastmoney/IndustryInfoLocalProcessor.java
+++ financehelper-samples/src/main/java/wangzhongqiu/financehelper/samples/processor/eastmoney/IndustryInfoLocalProcessor.java
@@ -1,69 +1,56 @@
 package wangzhongqiu.financehelper.samples.processor.eastmoney;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import us.codecraft.webmagic.Page;
-import us.codecraft.webmagic.Site;
-import us.codecraft.webmagic.Spider;
-import us.codecraft.webmagic.processor.PageProcessor;
+import org.springframework.stereotype.Component;
+import wangzhongqiu.financehelper.samples.dao.eastmoney.IndustryInfoDao;
 import wangzhongqiu.financehelper.samples.model.eastmoney.IndustryInfo;
-import wangzhongqiu.financehelper.samples.pipeline.eastmoney.IndustryInfoPipeline;
 import zhongqiu.javautils.DateUtil;
-import zhongqiu.javautils.JsonUtil;
+import zhongqiu.javautils.FileUtil;
 
+import javax.annotation.Resource;
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
+import java.util.TreeMap;
 
 /**
  * Created by wangzhongqiu on 2017/9/13.
  */
-public class IndustryInfoLocalProcessor implements PageProcessor {
-    private static String AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.57 Safari/537.36";
-    private static String URL = "http://nufm.dfcfw.com/EM_Finance2014NumericApplication/JS.aspx?cmd=C._BKHY&type=ct&sr=-1&ps=50&token=894050c76af8597a853f5b408b759f5d&sty=DCFFITABK&rt=50173371";
-    private Site site = Site.me().setRetryTimes(3).setSleepTime(1000).setUserAgent(AGENT);
-
-    @Override
-    public Site getSite() {
-        return site;
-    }
-
-    @Qualifier("IndustryInfoPipeline")
-    @Autowired
-    private IndustryInfoPipeline industryInfoPipeline;
+@Component
+public class IndustryInfoLocalProcessor {
+    @Resource
+    private IndustryInfoDao industryInfoDao;
 
     public void crawl() {
-        Spider.create(new IndustryInfoProcessor()).addUrl(URL).addPipeline(industryInfoPipeline).thread(5).run();
-    }
+        String file = "E:\\work\\githubcode\\financehelper\\financehelper-samples\\src\\main\\resources\\新建文本文档.txt";
+        String charsetName = "GB2312";
+        TreeMap<Integer, String> results = FileUtil.readFileByLines(file, charsetName);
+        for (int i = 1; i <= results.size(); i = i + 3) {
+            String date = results.get(i);
+            date = date.replace("【 ", "").replace(" 】", "");
+            date = DateUtil.dateToString(new Date(date), DateUtil.DATE_FORMAT_DAY_SHORT);
 
-    @Override
-    public void process(Page page) {
-        String now = DateUtil.dateToString(new Date(), DateUtil.DATE_FORMAT_DAY_SHORT);
-        String temp = page.getHtml().toString();
-        int beginIndex = temp.lastIndexOf('[');
-        int endIndex = temp.lastIndexOf(']');
-        temp = temp.substring(beginIndex, endIndex + 1);
-        List<Object> items = JsonUtil.jsonToList(temp, Object.class);
-        List<IndustryInfo> industryInfos = new ArrayList<IndustryInfo>();
-        for (Object object : items) {
-            String[] arr = object.toString().split(",");
-            IndustryInfo industryInfo = new IndustryInfo();
-            //// TODO: 2017/9/13 setIndustryId
-            industryInfo.setIndustryName(arr[2]);
-            industryInfo.setRise(new BigDecimal(arr[3]));
-            industryInfo.setMain(Integer.parseInt(arr[4].substring(0, arr[4].lastIndexOf("."))));
-            industryInfo.setSuper_(Integer.parseInt(arr[6].substring(0, arr[6].lastIndexOf("."))));
-            industryInfo.setBig(Integer.parseInt(arr[8].substring(0, arr[8].lastIndexOf("."))));
-            industryInfo.setMedium(Integer.parseInt(arr[10].substring(0, arr[10].lastIndexOf("."))));
-            industryInfo.setSmall(Integer.parseInt(arr[12].substring(0, arr[12].lastIndexOf("."))));
-            industryInfo.setTotal(industryInfo.getMain() + industryInfo.getSuper_() + industryInfo.getBig() + industryInfo.getMedium() + industryInfo.getSmall());
-            industryInfo.setDate(Integer.parseInt(now));
-            industryInfos.add(industryInfo);
+            /*String temp2 = results.get(i + 1);
+            String[] arrTemp2 = temp2.split(",");
+            for (String str : arrTemp2) {
+                IndustryInfo industryInfo = new IndustryInfo();
+                industryInfo.setDate(Integer.parseInt(date));
+                industryInfo.setIndustryName(str.substring(str.lastIndexOf("【") + 1, str.lastIndexOf("】")));
+                industryInfo.setRise(new BigDecimal(str.substring(str.lastIndexOf("】") + 1, str.length())));
+                industryInfoDao.add(industryInfo);
+            }
+            String temp3 = results.get(i + 2);
+            String[] arrTemp3 = temp3.split(",");
+            for (String str : arrTemp3) {
+                IndustryInfo industryInfo = new IndustryInfo();
+                industryInfo.setDate(Integer.parseInt(date));
+                industryInfo.setIndustryName(str.substring(str.lastIndexOf("【") + 1, str.lastIndexOf("】")));
+                Double d = Double.parseDouble(str.substring(str.lastIndexOf("】") + 1, str.length())) * 10000;
+                industryInfo.setTotal(d.intValue());
+                industryInfoDao.add(industryInfo);
+            }*/
+            System.out.println(date);
         }
-        page.putField("industryInfos", industryInfos);
     }
 
     public static void main(String[] args) {
--- sql/init.sql
+++ sql/init.sql
@@ -6,3 +6,20 @@ create table `Industry` (
   `urlMd5` varchar(200) not null default "",
   unique key `un_ix_url_md5` (`urlMd5`)
 ) default charset 'utf8' ENGINE='innodb';
+
+
+CREATE TABLE `industry_info` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `industry_name` varchar(200) NOT NULL DEFAULT '',
+  `industry_id` int(11) DEFAULT NULL,
+  `rise` decimal(6,2) DEFAULT NULL COMMENT '涨跌幅',
+  `main` int(11) DEFAULT NULL COMMENT '主力流入',
+  `super` int(11) DEFAULT NULL COMMENT '超大单流入',
+  `big` int(11) DEFAULT NULL COMMENT '大单流入',
+  `medium` int(11) DEFAULT NULL COMMENT '中单流入',
+  `small` int(11) DEFAULT NULL COMMENT '小单流入',
+  `total` int(11) DEFAULT NULL COMMENT '总流入',
+  `date` int(11) DEFAULT NULL COMMENT '日期',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uk_industry_id_date` (`industry_id`,`date`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=421 DEFAULT CHARSET=utf8;
