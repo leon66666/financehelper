e96aec9016c7c2b95323893086da8a7a8accc2d2
leon66666 <1902518544@qq.com>
2017-09-20 11:57:20 +0800

    second version

--- financehelper-samples/src/main/java/wangzhongqiu/financehelper/samples/dao/eastmoney/IndustryInfoDao.java
+++ financehelper-samples/src/main/java/wangzhongqiu/financehelper/samples/dao/eastmoney/IndustryInfoDao.java
@@ -1,8 +1,11 @@
 package wangzhongqiu.financehelper.samples.dao.eastmoney;
 
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 import wangzhongqiu.financehelper.samples.model.eastmoney.IndustryInfo;
+
+import java.util.List;
 
 /**
  * Created by wangzhongqiu on 2017/9/13.
@@ -11,4 +14,10 @@ import wangzhongqiu.financehelper.samples.model.eastmoney.IndustryInfo;
 public interface IndustryInfoDao {
     @Insert("insert into industry_info (`industry_name`,`rise`,`main`,`super`,`big`,`medium`,`small`,`total`,`date`) values (#{industryName},#{rise},#{main},#{super_},#{big},#{medium},#{small},#{total},#{date})")
     public int add(IndustryInfo industryInfo);
+
+    @Select("select * from industry_info where industry_name in(${industrys})")
+    public List<IndustryInfo> get(List<String> industrys);
+
+    @Select("select * from industry_info where industry_name=#{industry} order by date asc")
+    public List<IndustryInfo> get(String industry);
 }
--- financehelper-samples/src/test/java/wangzhongqiu/financehelper/samples/test/DrawingTest.java
+++ financehelper-samples/src/test/java/wangzhongqiu/financehelper/samples/test/DrawingTest.java
@@ -1,22 +1,37 @@
 package wangzhongqiu.financehelper.samples.test;
 
+import org.junit.Test;
+import org.springframework.stereotype.*;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.ContextConfiguration;
 import wangzhongqiu.financehelper.samples.drawing.BarChart;
 import wangzhongqiu.financehelper.samples.drawing.PieChart;
 import wangzhongqiu.financehelper.samples.drawing.TimeSeriesChart;
+import wangzhongqiu.financehelper.samples.drawing.eastmoney.IndustryTimeSeriesChart;
 
+import javax.annotation.Resource;
 import javax.swing.*;
 import java.awt.*;
+import java.util.*;
 
 /**
  * Created by wangzhongqiu on 2017/9/19.
  */
+@ContextConfiguration(locations = "classpath:/spring/applicationContextTest.xml")
 public class DrawingTest {
-    public static void main(String args[]) {
+    @Resource
+    private IndustryTimeSeriesChart industryTimeSeriesChart;
+
+    @Test
+    public void testRun() {
         JFrame frame = new JFrame("数据统计图");
         frame.setLayout(new GridLayout(2, 2, 10, 10));
 //        frame.add(new BarChart().getChartPanel());           //添加柱形图
 //        frame.add(new PieChart().getChartPanel());           //添加饼状图
-        frame.add(new TimeSeriesChart().getChartPanel());    //添加折线图
+//        frame.add(new TimeSeriesChart().getChartPanel());    //添加折线图
+        java.util.List<String> industrys = new ArrayList<String>();
+        industrys.add("房地产");
+        frame.add(industryTimeSeriesChart.getChartPanel(industrys));
         frame.setBounds(50, 50, 800, 600);
         frame.setVisible(true);
     }
